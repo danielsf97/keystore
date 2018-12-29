512d2b99dd0f279cae94a134ab310c9eb7d6338e
mm97 <marianasamiranda@gmail.com>
2018-12-29 18:05:59 +0000

    O

--- keystore/src/main/java/keystore/KeystoreSrv.java
+++ keystore/src/main/java/keystore/KeystoreSrv.java
@@ -59,7 +59,6 @@ public class KeystoreSrv  {
         ms.registerHandler(TwoPCProtocol.GetControllerReq.class.getName(), this::get, es);
 
         ms.registerHandler(TwoPCProtocol.ControllerAbortReq.class.getName(), this::abort, es);
-        //TODO: aqui ir buscar a informação ao Log
 
         System.out.println("Size: " + data.size());
 
@@ -100,24 +99,17 @@ public class KeystoreSrv  {
         System.out.println("ABORT");
         TwoPCProtocol.ControllerAbortReq ab = s.decode(bytes);
         int trans_id = ab.txId;
-        if(log.actionAlreadyExists(trans_id, Phase.ROLLBACKED)){
-
+        if(!pendent_puts.containsKey(trans_id)){
             TwoPCProtocol.ControllerAbortResp p = new TwoPCProtocol.ControllerAbortResp(trans_id,myId);
             ms.sendAsync(address, TwoPCProtocol.ControllerAbortResp.class.getName(),s.encode(p));
-            System.out.println("Transaction " + trans_id + " already rollbacked!!");
+            System.out.println("Transaction " + trans_id + " already doesn't exist!!");
         }
-        else if (log.actionAlreadyExists(trans_id, Phase.PREPARED)) {
+        else {
             log.write(trans_id, Phase.ROLLBACKED.toString());
             pendent_puts.remove(trans_id);
             TwoPCProtocol.ControllerAbortResp p = new TwoPCProtocol.ControllerAbortResp(trans_id,myId);
             ms.sendAsync(address, TwoPCProtocol.ControllerAbortResp.class.getName(),s.encode(p));
             System.out.println("Transaction " + trans_id + " rollbacked from prepared!!");
-        }
-        else {
-            log.write(trans_id, Phase.ROLLBACKED.toString());
-            TwoPCProtocol.ControllerAbortResp p = new TwoPCProtocol.ControllerAbortResp(trans_id,myId);
-            ms.sendAsync(address, TwoPCProtocol.ControllerAbortResp.class.getName(),s.encode(p));
-            System.out.println("Transaction " + trans_id + " rollbacked from commit!!");
         }
     }
 
@@ -136,21 +128,21 @@ public class KeystoreSrv  {
     }
 
 
-    //TODO: MUDAR DE LOG PARA MAP
+    // Note: É possivel receber um pedido de prepared em algo que ele já fez abort????
     private void putTwoPC1(Address address, byte[] m){
         System.out.println("PUT in keystore");
         TwoPCProtocol.ControllerPreparedReq prepReq = s.decode(m);
         int trans_id = prepReq.txId;
-        if(log.actionAlreadyExists(trans_id, Phase.PREPARED) ){
-            System.out.println("OO");
+        if(pendent_puts.containsKey(trans_id)){
+            System.out.println("Already sent prepared");
             TwoPCProtocol.ControllerPreparedResp p = new TwoPCProtocol.ControllerPreparedResp(trans_id,myId);
             ms.sendAsync(address, TwoPCProtocol.ControllerPreparedResp.class.getName(),s.encode(p));
         }
-        else if (log.actionAlreadyExists(trans_id, Phase.ROLLBACKED)){
+     /*   else if (log.actionAlreadyExists(trans_id, Phase.ROLLBACKED)){
             TwoPCProtocol.ControllerAbortResp p = new TwoPCProtocol.ControllerAbortResp(trans_id,myId);
             ms.sendAsync(address, TwoPCProtocol.ControllerAbortResp.class.getName(),s.encode(p));
-        }
-        else if(!log.actionAlreadyExists(trans_id, Phase.PREPARED) && !log.actionAlreadyExists(trans_id, Phase.ROLLBACKED)) {
+        }*/
+        else {
             System.out.print("You prepared for transaction " + trans_id + "?");
             String line = sc.nextLine();
             if (line != null && line.equals("yes")) {
@@ -171,12 +163,12 @@ public class KeystoreSrv  {
     private void putTwoPC2(Address address, byte[] m) {
         TwoPCProtocol.ControllerCommitReq commitReq = s.decode(m);
         int trans_id = commitReq.txId;
-        if (log.actionAlreadyExists(trans_id, Phase.COMMITED)){
+        if (pendent_puts.containsKey(trans_id)){ // se estiver committed é porque não existe nos pendent_puts
             System.out.println("OO");
             TwoPCProtocol.ControllerCommitedResp p = new TwoPCProtocol.ControllerCommitedResp(trans_id,myId);
             ms.sendAsync(address, TwoPCProtocol.ControllerCommitedResp.class.getName(),s.encode(p));
         }
-        else if(!log.actionAlreadyExists(trans_id, Phase.COMMITED)) {
+        else {
             data.putAll(pendent_puts.get(trans_id));
             pendent_puts.remove(trans_id);
             log.write(trans_id, Phase.COMMITED.toString());
